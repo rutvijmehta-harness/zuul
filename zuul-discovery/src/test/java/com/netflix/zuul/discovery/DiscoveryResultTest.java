f3db6c2b81a78f0ce99bc0e6c386eb66d4340e03
Rutvij Mehta <rutvij.mehta@harness.io>
2024-06-20 02:53:32 -0700

    Config changes

--- zuul-discovery/src/test/java/com/netflix/zuul/discovery/DiscoveryResultTest.java
+++ zuul-discovery/src/test/java/com/netflix/zuul/discovery/DiscoveryResultTest.java
@@ -15,7 +15,7 @@
  */
 
 package com.netflix.zuul.discovery;
-
+// test
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
